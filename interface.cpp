0f435161c87e17dac35bb67440827ed78ce5c840
Eric Jensen <jensen39@gmail.com>
2010-11-22 01:10:03 +0000

    Clean-up of interface.
    git-svn-id: file:///Users/Eric/Developer/Other/YetiCodecSVN/trunk@39 a9bbc950-3a31-49f7-86fe-bb7552e34ce9

--- interface.cpp
+++ interface.cpp
@@ -16,37 +16,37 @@ CodecInst::CodecInst()
    }
 #endif
 
-   m_compressWorker.m_probRanges=NULL;
-   m_compressWorker.m_bytecounts=NULL;
-   m_compressWorker.m_buffer=NULL;
-   m_buffer=NULL;
-   m_prevFrame=NULL;
-   m_deltaBuffer=NULL;
-   m_buffer2=NULL;
-   m_colorTransBuffer=NULL;
-   m_length=0;
-   m_nullframes=true;
-   m_deltaframes=true;
-   m_reduced=false;
-   m_started=false;
-   m_SSE2=0;
-   m_SSE=0;
+   m_compressWorker.m_probRanges = NULL;
+   m_compressWorker.m_bytecounts = NULL;
+   m_compressWorker.m_buffer = NULL;
+   m_buffer = NULL;
+   m_prevFrame = NULL;
+   m_deltaBuffer = NULL;
+   m_buffer2 = NULL;
+   m_colorTransBuffer = NULL;
+   m_length = 0;
+   m_nullframes = false;
+   m_deltaframes = false;
+   m_reduced = false;
+   m_started = false;
+   m_SSE2 = 0;
+   m_SSE = 0;
 
-   m_multithreading=0;
-   m_info_a.m_source=NULL;
-   m_info_a.m_dest=NULL;
-   m_info_a.m_size=0;
-   m_info_a.m_length=0;
-   m_info_a.m_thread=0;
-   m_info_b=m_info_a;
-   m_info_c=m_info_a;
+   m_multithreading = 0;
+   m_info_a.m_source = NULL;
+   m_info_a.m_dest = NULL;
+   m_info_a.m_size = 0;
+   m_info_a.m_length = 0;
+   m_info_a.m_thread = 0;
+   m_info_b = m_info_a;
+   m_info_c = m_info_a;
 
-   memcpy((void*)&m_info_a.m_compressWorker,&m_compressWorker,sizeof(m_compressWorker));
-   memcpy((void*)&m_info_b.m_compressWorker,&m_compressWorker,sizeof(m_compressWorker));
-   memcpy((void*)&m_info_c.m_compressWorker,&m_compressWorker,sizeof(m_compressWorker));
+   memcpy((void*)&m_info_a.m_compressWorker, &m_compressWorker, sizeof(m_compressWorker));
+   memcpy((void*)&m_info_b.m_compressWorker, &m_compressWorker, sizeof(m_compressWorker));
+   memcpy((void*)&m_info_c.m_compressWorker, &m_compressWorker, sizeof(m_compressWorker));
 }
 
-HMODULE hmoduleYeti=0;
+HMODULE hmoduleYeti = 0;
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD, LPVOID) 
 {
@@ -56,30 +56,30 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD, LPVOID)
 
 static BOOL CALLBACK ConfigureDialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-   if (uMsg == WM_INITDIALOG)
+   if(uMsg == WM_INITDIALOG)
    {
       CheckDlgButton(hwndDlg, IDC_NULLFRAMES,GetPrivateProfileInt("settings", "nullframes", false, SettingsFile) ? BST_CHECKED : BST_UNCHECKED);
       CheckDlgButton(hwndDlg, IDC_MULTI,GetPrivateProfileInt("settings", "multithreading", false, SettingsFile) ? BST_CHECKED : BST_UNCHECKED);
       CheckDlgButton(hwndDlg, IDC_REDUCED,GetPrivateProfileInt("settings", "reduced", false, SettingsFile) ? BST_CHECKED : BST_UNCHECKED);
       CheckDlgButton(hwndDlg, IDC_DELTAFRAMES,GetPrivateProfileInt("settings", "deltaframes", false, SettingsFile) ? BST_CHECKED : BST_UNCHECKED);
    } 
-   else if (uMsg == WM_COMMAND) 
+   else if(uMsg == WM_COMMAND) 
    {
-      if (LOWORD(wParam)==IDC_OK)
+      if(LOWORD(wParam)==IDC_OK)
       {
-         WritePrivateProfileString("settings", "nullframes",(IsDlgButtonChecked(hwndDlg, IDC_NULLFRAMES) == BST_CHECKED) ? "1" : NULL, SettingsFile);
-         WritePrivateProfileString("settings", "multithreading",(IsDlgButtonChecked(hwndDlg, IDC_MULTI) == BST_CHECKED) ? "1" : NULL, SettingsFile);
-         WritePrivateProfileString("settings", "reduced",(IsDlgButtonChecked(hwndDlg, IDC_REDUCED) == BST_CHECKED) ? "1" : NULL, SettingsFile);
-         WritePrivateProfileString("settings", "deltaframes",(IsDlgButtonChecked(hwndDlg, IDC_DELTAFRAMES) == BST_CHECKED) ? "1" : NULL, SettingsFile);
+         WritePrivateProfileString("settings", "nullframes", (IsDlgButtonChecked(hwndDlg, IDC_NULLFRAMES) == BST_CHECKED) ? "1" : NULL, SettingsFile);
+         WritePrivateProfileString("settings", "multithreading", (IsDlgButtonChecked(hwndDlg, IDC_MULTI) == BST_CHECKED) ? "1" : NULL, SettingsFile);
+         WritePrivateProfileString("settings", "reduced", (IsDlgButtonChecked(hwndDlg, IDC_REDUCED) == BST_CHECKED) ? "1" : NULL, SettingsFile);
+         WritePrivateProfileString("settings", "deltaframes", (IsDlgButtonChecked(hwndDlg, IDC_DELTAFRAMES) == BST_CHECKED) ? "1" : NULL, SettingsFile);
 
          EndDialog(hwndDlg, 0);
       } 
-      else if ( LOWORD(wParam)==IDC_CANCEL )
+      else if(LOWORD(wParam)==IDC_CANCEL)
       {
          EndDialog(hwndDlg, 0);
       } 
    } 
-   else if ( uMsg == WM_CLOSE )
+   else if(uMsg == WM_CLOSE)
    {
       EndDialog(hwndDlg, 0);
    }
@@ -99,14 +99,14 @@ DWORD CodecInst::Configure(HWND hwnd)
 
 CodecInst* Open(ICOPEN* icinfo) 
 {
-   if (icinfo && icinfo->fccType != ICTYPE_VIDEO)
+   if(icinfo && icinfo->fccType != ICTYPE_VIDEO)
    {
       return NULL;
    }
 
    CodecInst* pinst = new CodecInst();
 
-   if (icinfo) 
+   if(icinfo) 
    {
       icinfo->dwError = pinst ? ICERR_OK : ICERR_MEMORY;
    }
@@ -118,7 +118,7 @@ CodecInst::~CodecInst()
 {
    try
    {
-      if ( m_started )
+      if(m_started)
       {
          if(m_colorTransBuffer)
          {
@@ -138,7 +138,7 @@ DWORD Close(CodecInst* pinst)
 {
    try 
    {
-      if ( pinst && !IsBadWritePtr(pinst,sizeof(CodecInst)) )
+      if(pinst && !IsBadWritePtr(pinst,sizeof(CodecInst)))
       {
          delete pinst;
       }
@@ -149,26 +149,26 @@ DWORD Close(CodecInst* pinst)
 
 DWORD CodecInst::GetState(LPVOID pv, DWORD dwSize)
 {
-   if ( pv == NULL )
+   if(pv == NULL)
    {
       return 4 * sizeof(int);
    } 
-   else if ( dwSize < 4 * sizeof(int) )
+   else if(dwSize < 4 * sizeof(int))
    {
       return (DWORD)ICERR_BADSIZE;
    }
 
    int * state = (int*)pv;
-   state[0] = GetPrivateProfileInt("settings", "nullframes", false, SettingsFile);
-   state[1] = GetPrivateProfileInt("settings", "multithreading", false, SettingsFile);
+   state[0] = GetPrivateProfileInt("settings", "nullframes", true, SettingsFile);
+   state[1] = GetPrivateProfileInt("settings", "multithreading", true, SettingsFile);
    state[2] = GetPrivateProfileInt("settings", "reduced", false, SettingsFile);
-   state[3] = GetPrivateProfileInt("settings", "deltaframes", false, SettingsFile);
+   state[3] = GetPrivateProfileInt("settings", "deltaframes", true, SettingsFile);
    return 0;
 }
 
 DWORD CodecInst::SetState(LPVOID pv, DWORD dwSize) 
 {
-   if ( dwSize < 4 * sizeof(int))
+   if(dwSize < 4 * sizeof(int))
    {
       return 4 * sizeof(int);
    }
@@ -211,25 +211,25 @@ DWORD CodecInst::CompressQuery(LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER lpb
 {
 
    // check for valid format and bitdepth
-   if ( lpbiIn->biCompression == 0) 
+   if(lpbiIn->biCompression == 0) 
    {
-      if ( lpbiIn->biBitCount != 24 && lpbiIn->biBitCount != 32)
+      if(lpbiIn->biBitCount != RGB24 && lpbiIn->biBitCount != RGB32)
       {
-         RETURN_ERROR()
+         RETURN_ERROR();
       }
    } 
-   else if ( lpbiIn->biCompression == FOURCC_YUY2 )
+   else if(lpbiIn->biCompression == FOURCC_YUY2)
    {
-      if ( lpbiIn->biBitCount != 16 ) 
+      if(lpbiIn->biBitCount != YUY2) 
       {
-         RETURN_ERROR()
+         RETURN_ERROR();
       }
    }
-   else if ( lpbiIn->biCompression == FOURCC_YV12 )
+   else if(lpbiIn->biCompression == FOURCC_YV12)
    {
-      if ( lpbiIn->biBitCount != 12 )
+      if(lpbiIn->biBitCount != YV12)
       {
-         RETURN_ERROR()
+         RETURN_ERROR();
       }
    } 
    else 
@@ -239,38 +239,33 @@ DWORD CodecInst::CompressQuery(LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER lpb
       char * y = (char*)&x;
       sprintf(msg,"Unknown format, %c%c%c%c",y[0],y[1],y[2],y[3]);
       MessageBox (HWND_DESKTOP, msg, "Error", MB_OK | MB_ICONEXCLAMATION);*/
-      RETURN_ERROR()
-   }
-
-   // Make sure width is mod 4 for YUV formats
-   if ( lpbiIn->biWidth % 4 )
-   {
-      RETURN_ERROR()
-   }
-
-   // Make sure the height is acceptable for YV12 formats
-   if ( lpbiIn->biHeight % 2)
-   {
       RETURN_ERROR();
    }
 
+   // Make sure width is mod 4 for YUV formats
+   if(lpbiIn->biWidth % 4)
+      RETURN_ERROR();
+   // Make sure the height is acceptable for YV12 formats
+   if(lpbiIn->biHeight % 2)
+      RETURN_ERROR();
+
    // See if the output format is acceptable if need be
-   if ( lpbiOut )
+   if(lpbiOut)
    {
-      if ( lpbiOut->biSize < sizeof(BITMAPINFOHEADER) )
-         RETURN_ERROR()
-         if ( lpbiOut->biBitCount != 24 && lpbiOut->biBitCount != 32 && lpbiOut->biBitCount != 16 && lpbiOut->biBitCount != 12 )
-            RETURN_ERROR()
-            if ( lpbiIn->biHeight != lpbiOut->biHeight )
-               RETURN_ERROR()
-               if ( lpbiIn->biWidth != lpbiOut->biWidth )
-                  RETURN_ERROR()
-                  if ( lpbiOut->biCompression != FOURCC_YETI )
-                     RETURN_ERROR()
+      if(lpbiOut->biSize < sizeof(BITMAPINFOHEADER))
+         RETURN_ERROR();
+      if(lpbiOut->biBitCount != RGB24 && lpbiOut->biBitCount != RGB32 && lpbiOut->biBitCount != YUY2 && lpbiOut->biBitCount != YV12)
+         RETURN_ERROR();
+      if(lpbiIn->biHeight != lpbiOut->biHeight)
+         RETURN_ERROR();
+      if(lpbiIn->biWidth != lpbiOut->biWidth)
+         RETURN_ERROR();
+      if(lpbiOut->biCompression != FOURCC_YETI)
+         RETURN_ERROR();
    }
 
    detectFlags(&m_SSE2, &m_SSE);
-   if ( !m_SSE )
+   if(!m_SSE)
    {
       RETURN_ERROR()
    }
@@ -281,15 +276,15 @@ DWORD CodecInst::CompressQuery(LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER lpb
 // return the intended compress format for the given input format
 DWORD CodecInst::CompressGetFormat(LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER lpbiOut)
 {
-   if ( !lpbiOut)
+   if(!lpbiOut)
    {
       return sizeof(BITMAPINFOHEADER)+sizeof(UINT32);	
    }
 
    // make sure the input is an acceptable format
-   if ( CompressQuery(lpbiIn, NULL) == ICERR_BADFORMAT )
+   if(CompressQuery(lpbiIn, NULL) == ICERR_BADFORMAT)
    {
-      RETURN_ERROR()
+      RETURN_ERROR();
    }
 
    *lpbiOut = *lpbiIn;
@@ -297,7 +292,7 @@ DWORD CodecInst::CompressGetFormat(LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER
    lpbiOut->biPlanes = 1;
    lpbiOut->biCompression = FOURCC_YETI;
 
-   if ( lpbiIn->biBitCount != 24 )
+   if(lpbiIn->biBitCount != RGB24)
    {
       lpbiOut->biSizeImage = EIGHTH(lpbiIn->biWidth * lpbiIn->biHeight * lpbiIn->biBitCount);
       lpbiOut->biSizeImage = EIGHTH(lpbiIn->biWidth * lpbiIn->biHeight * lpbiIn->biBitCount);
@@ -309,19 +304,18 @@ DWORD CodecInst::CompressGetFormat(LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER
 
    lpbiOut->biBitCount = lpbiIn->biBitCount;
 
-   //*(UINT32*)(&lpbiOut[1])= m_reduced ? REDUCED_RES : YV12_FRAME; //Change?
    return (DWORD)ICERR_OK;
 }
 
 // return information about the codec
 DWORD CodecInst::GetInfo(ICINFO* icinfo, DWORD dwSize)
 {
-   if (icinfo == NULL)
+   if(icinfo == NULL)
    {
       return sizeof(ICINFO);
    }
 
-   if (dwSize < sizeof(ICINFO))
+   if(dwSize < sizeof(ICINFO))
    {
       return 0;
    }
@@ -329,8 +323,8 @@ DWORD CodecInst::GetInfo(ICINFO* icinfo, DWORD dwSize)
    icinfo->dwSize       = sizeof(ICINFO);
    icinfo->fccType      = ICTYPE_VIDEO;
    icinfo->fccHandler	= FOURCC_YETI;
-   icinfo->dwFlags		=  VIDCF_FASTTEMPORALC | VIDCF_FASTTEMPORALD | VIDCF_TEMPORAL;
-   icinfo->dwVersion		= 0x00010000;
+   icinfo->dwFlags		= VIDCF_FASTTEMPORALC | VIDCF_FASTTEMPORALD | VIDCF_TEMPORAL;
+   icinfo->dwVersion		= 0x00000001;
    icinfo->dwVersionICM	= ICVERSION;
    memcpy(icinfo->szName, L"Yeti", sizeof(L"Yeti"));
    memcpy(icinfo->szDescription, L"Yeti Video Codec", sizeof(L"Yeti Video Codec"));
@@ -341,36 +335,32 @@ DWORD CodecInst::GetInfo(ICINFO* icinfo, DWORD dwSize)
 // check if the codec can decompress the given format to the desired format
 DWORD CodecInst::DecompressQuery(const LPBITMAPINFOHEADER lpbiIn, const LPBITMAPINFOHEADER lpbiOut)
 {
-   if ( lpbiIn->biCompression != FOURCC_YETI )
+   if(lpbiIn->biCompression != FOURCC_YETI)
    {
-      return (DWORD)ICERR_BADFORMAT;
+      RETURN_ERROR();
    }
 
-   if ( lpbiIn->biBitCount == 16 || lpbiIn->biBitCount == 12 )
+   if(lpbiIn->biBitCount == YUY2 || lpbiIn->biBitCount == YV12)
    {
-      if ( lpbiIn->biWidth%4 )
-      {
-         return (DWORD)ICERR_BADFORMAT;
-      }
-      if ( lpbiIn->biBitCount == 12 && lpbiIn->biHeight%2 )
-      {
-         return (DWORD)ICERR_BADFORMAT;
-      }
+      if(lpbiIn->biWidth % 4)
+         RETURN_ERROR();
+      if(lpbiIn->biBitCount == YV12 && lpbiIn->biHeight % 2)
+         RETURN_ERROR();
    }
 
    // make sure the input bitdepth is valid
-   if ( lpbiIn->biBitCount != 24 && lpbiIn->biBitCount != 32 && lpbiIn->biBitCount != 16 && lpbiIn->biBitCount != 12 )
+   if(lpbiIn->biBitCount != RGB24 && lpbiIn->biBitCount != RGB32 && lpbiIn->biBitCount != YUY2 && lpbiIn->biBitCount != YV12)
    {
-      return (DWORD)ICERR_BADFORMAT;
+      RETURN_ERROR();
    }
 
    detectFlags(&m_SSE2, &m_SSE);
-   if ( !m_SSE )
+   if(!m_SSE)
    {
-      RETURN_ERROR()
+      RETURN_ERROR();
    }
 
-   if ( !lpbiOut )
+   if(!lpbiOut)
    {
       return (DWORD)ICERR_OK;
    }
@@ -382,53 +372,24 @@ DWORD CodecInst::DecompressQuery(const LPBITMAPINFOHEADER lpbiIn, const LPBITMAP
    //MessageBox (HWND_DESKTOP, msg, "Error", MB_OK | MB_ICONEXCLAMATION);
 
    // make sure the output format is one that can be decoded to
-   if ( lpbiOut->biCompression != 0 && lpbiOut->biCompression != FOURCC_YV12 && lpbiOut->biCompression != FOURCC_YUY2 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-
-   // make sure the output bitdepth is valid
-   if ( lpbiOut->biBitCount != 32 && lpbiOut->biBitCount != 24 && lpbiOut->biBitCount != 16 && lpbiOut->biBitCount != 12 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-   // make sure that no down sampling is being performed
-   if ( lpbiOut->biBitCount < 12 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-
-   // check for invalid widths/heights
-   if ( lpbiIn->biWidth % 4 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-   if ( lpbiIn->biHeight % 2 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-
-   if ( lpbiOut->biCompression == 0 && lpbiOut->biBitCount != 24 && lpbiOut->biBitCount != 32)
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-   if ( lpbiOut->biCompression == FOURCC_YUY2 && lpbiOut->biBitCount != 16 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-   if ( lpbiOut->biCompression == FOURCC_YV12 && lpbiOut->biBitCount != 12 )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-
-   if ( lpbiIn->biHeight != lpbiOut->biHeight )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
-   if ( lpbiIn->biWidth != lpbiOut->biWidth )
-   {
-      return (DWORD)ICERR_BADFORMAT;
-   }
+   if(lpbiOut->biCompression != 0 && lpbiOut->biCompression != FOURCC_YV12 && lpbiOut->biCompression != FOURCC_YUY2)
+      RETURN_ERROR();
+   if(lpbiOut->biBitCount != RGB32 && lpbiOut->biBitCount != RGB24 && lpbiOut->biBitCount != YUY2 && lpbiOut->biBitCount != YV12)   // make sure the output bitdepth is valid
+      RETURN_ERROR();
+   if(lpbiIn->biWidth % 4) // check for invalid widths/heights
+      RETURN_ERROR();
+   if(lpbiIn->biHeight % 2)
+      RETURN_ERROR();
+   if(lpbiOut->biCompression == 0 && lpbiOut->biBitCount != RGB24 && lpbiOut->biBitCount != RGB32)
+      RETURN_ERROR();
+   if(lpbiOut->biCompression == FOURCC_YUY2 && lpbiOut->biBitCount != YUY2)
+      RETURN_ERROR();
+   if(lpbiOut->biCompression == FOURCC_YV12 && lpbiOut->biBitCount != YV12)
+      RETURN_ERROR();
+   if(lpbiIn->biHeight != lpbiOut->biHeight)
+      RETURN_ERROR();
+   if(lpbiIn->biWidth != lpbiOut->biWidth)
+      RETURN_ERROR();
 
    return (DWORD)ICERR_OK;
 }
@@ -436,12 +397,12 @@ DWORD CodecInst::DecompressQuery(const LPBITMAPINFOHEADER lpbiIn, const LPBITMAP
 // return the default decompress format for the given input format 
 DWORD CodecInst::DecompressGetFormat(const LPBITMAPINFOHEADER lpbiIn, LPBITMAPINFOHEADER lpbiOut)
 {
-   if ( DecompressQuery(lpbiIn, NULL ) != ICERR_OK)
+   if(DecompressQuery(lpbiIn, NULL) != ICERR_OK)
    {
-      return (DWORD)ICERR_BADFORMAT;
+      RETURN_ERROR();
    }
 
-   if ( !lpbiOut)
+   if(!lpbiOut)
    {
       return sizeof(BITMAPINFOHEADER);
    }

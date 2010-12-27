877a36de22b4703ced1b47bb39c441e586afa287
Eric Jensen <jensen39@gmail.com>
2010-12-27 03:39:00 +0000

    Added common.h
    git-svn-id: file:///Users/Eric/Developer/Other/YetiCodecSVN/trunk@57 a9bbc950-3a31-49f7-86fe-bb7552e34ce9

---
+++ common.h
@@ -0,0 +1,15 @@
+#pragma once
+
+typedef unsigned char BYTE;
+
+#define SWAP(x, y) { BYTE xchng = *(BYTE*)(&x); x = y; *(BYTE*)(&y) = xchng; }
+
+// y must be 2^n
+#define ALIGN_ROUND(x, y) ((((unsigned int)(x)) + (y - 1))&(~(y - 1)))
+
+#define HALF(x) (x>>1)
+#define FOURTH(x) (x>>2)
+#define EIGHTH(x) (x>>3)
+
+#define DOUBLE(x) (x<<1)
+#define QUADRUPLE(x) (x<<2)

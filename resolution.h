6383c27e9b6328d55b6011bcfa5092b27e80b354
Eric Jensen <jensen39@gmail.com>
2011-03-31 04:10:33 +0000

    Deleting stuff
    git-svn-id: file:///Users/Eric/Developer/Other/YetiCodecSVN/trunk@66 a9bbc950-3a31-49f7-86fe-bb7552e34ce9

--- resolution.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void ReduceRes(const BYTE* src, BYTE* dest, BYTE* buffer, const unsigned int width, const unsigned int height, const bool SSE2);
-void EnlargeRes(const BYTE* src, BYTE* dst, BYTE* buffer, unsigned int width, unsigned int height, const bool SSE2);
